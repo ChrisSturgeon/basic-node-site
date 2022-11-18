c05406725dab653d098abc7aefd5616c9bf0525a
csturgeon <sturgeon.chris@gmail.com>
2022-11-18 15:14:02 +0000

    Update file paths

--- index.js
+++ index.js
@@ -3,32 +3,33 @@ const fs = require('fs');
 
 http
   .createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-
     // Get path from users request
     const path = req.url;
 
     // Read and write relevant html file, or return 404 if no matching path
     if (path === '/') {
-      fs.readFile('./index.html', (err, data) => {
+      fs.readFile('index.html', (err, data) => {
         if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         return res.end();
       });
     } else if (path === '/about') {
-      fs.readFile('./about.html', (err, data) => {
+      fs.readFile('about.html', (err, data) => {
         if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         return res.end();
       });
     } else if (path === '/contact-me') {
-      fs.readFile('./contact-me.html', (err, data) => {
+      fs.readFile('contact-me.html', (err, data) => {
         if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         return res.end();
       });
     } else {
-      fs.readFile('./404.html', (err, data) => {
+      fs.readFile('404.html', (err, data) => {
         if (err) throw err;
         res.writeHead(404, { 'Content-Type': 'text/html' });
         res.write(data);
